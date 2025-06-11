f2ec671c2e40b8e7d8e42733f38f99d5c0bcace6
alef-ss <alef.ss@pronton.me>
2025-06-11 11:24:49 -0300

    adicionei session

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request, flash, redirect
+from flask import Flask, render_template, url_for, request, flash, redirect, session
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 import time
@@ -37,6 +37,11 @@ def index():
     return render_template('index.html')
 @app.route('/home')
 def home():
+    nome = session.get('nome')
+    if nome:
+        return render_template('home.html')
+    else:
+        print(f'Você precisa fazer login para acessar esta área!', redirect(url_for('entrar')))
     return render_template('home.html')
 
 # Rota pra página de cadastro
@@ -46,16 +51,21 @@ def cadastro():
         nome = request.form['nome']
         senha = request.form['senha']
 
-        # criptografar a senha e mandar a senha criptografada no lugar da senha "crua" enviada pelo usuário
-        # senhaCriptografada = bcrypt.generate_password_hash(criptografarSenha(senha))
+        """ criptografar a senha e mandar a senha criptografada no lugar da senha "crua" enviada pelo usuário
+        senhaCriptografada = bcrypt.generate_password_hash(criptografarSenha(senha)) """
+
         novo_usuario = Usuario(nome=nome, senha=criptografarSenha(senha))
         
-        # fiz uma tratativa de erro pra se der merda não mostre as mensagens de erro normais
+        """ fiz uma tratativa de erro pra se der merda
+        não mostre as mensagens de erro normais """
         
         try:
             db.session.add(novo_usuario)
             db.session.commit()
-            # adicionei o método flash pra armazenar as mensagens de sucesso ou erro pra mostrar pro usuário quando precisar
+
+            """ adicionei o método flash pra armazenar as mensagens
+            de sucesso ou erro pra mostrar pro usuário quando precisar """
+
             flash('Dados registrados!', 'success')
             return render_template('cadastro.html')
         except Exception as e:
@@ -75,6 +85,9 @@ def entrar():
             nome = request.form.get('nome')
             senha = request.form.get('senha')
             
+            session['nome'] = request.form['nome']
+            return redirect(url_for('home'))
+            
             usuario = Usuario.query.filter_by(nome=nome).first()
             
             if usuario and bcrypt.check_password_hash(usuario.senha, senha):
@@ -85,6 +98,11 @@ def entrar():
         flash(f'Erro ao logar ::::::::: {e}')
         
     return render_template('entrar.html')
+
+@app.route('/sair')
+def sair():
+    session.pop('nome', None)
+    return redirect(url_for('entrar'))
 
 if __name__ == '__main__':
     with app.app_context():
